69b59abccabf291874e5e2ec317cbba9802c35f6
chroneru <saturniidae.oak@gmail.com>
2018-07-08 19:27:05 +0900

    These are for HakodateR_0, output pdf is failed

---
+++ _common.R
@@ -0,0 +1,16 @@
+
+knitr::opts_chunk$set(
+    comment = "#>",
+    collapse = TRUE,
+    cache = TRUE,
+    fig.show = "hold",
+    out.width = "70%",
+    fig.align = "center",
+    fig.width = 6,
+    fig.asp = 0.618
+)
+
+library(tidyverse)
+library(DiagrammeR)
+library(leaflet)
+library(shiny)
---
+++ render_book.R
@@ -0,0 +1,14 @@
+#すべてのRmdをまとめる
+bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook")
+
+#pdf
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book")
+
+#_bookdown.yamlで指定したRmdをまとめる
+bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", config_file = "_bookdown.yml")
+
+#pdf
+bookdown::render_book("index.Rmd", output_format = "bookdown::pdf_book", config_file = "_bookdown.yml")
+
+#epub
+bookdown::render_book("index.Rmd", output_format = "bookdown::epub_book", config_file = "_bookdown.yml")
